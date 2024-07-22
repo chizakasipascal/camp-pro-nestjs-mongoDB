3a56e8bd02401da265429bd1e21f91e9f23aca8a
chizakasipascal <pascalkasichiza@gmail.com>
2024-07-22 23:23:33 +0200

    create crud oprations mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { SongsModule } from './songs/songs.module';
+import { AlbumsModule } from './albums/albums.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/spoty')],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/spoty'), SongsModule, AlbumsModule,],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/songs/songs.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { SongsController } from './songs.controller';
+import { SongsService } from './songs.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Song, SongSchema } from './schema/song';
+
+
+@Module({
+  imports: [MongooseModule.forFeature([{ name: Song.name, schema: SongSchema }])],
+  controllers: [SongsController],
+  providers: [SongsService]
+})
+export class SongsModule { }
